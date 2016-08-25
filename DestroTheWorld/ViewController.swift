ecd8383a20f8e2e75c398f9fc52bfed4c61faa2d
Hacene Benzekri <hacenebenzekri@hacenes-imac.lan>
2016-08-25 19:31:52 +0100

    Initial Commit

---
+++ DestroTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroTheWorld
+//
+//  Created by Hacene Benzekri on 25/08/2016.
+//  Copyright © 2016 Hacene Benzekri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DestroTheWorldTests/DestroTheWorldTests.swift
@@ -0,0 +1,36 @@
+//
+//  DestroTheWorldTests.swift
+//  DestroTheWorldTests
+//
+//  Created by Hacene Benzekri on 25/08/2016.
+//  Copyright © 2016 Hacene Benzekri. All rights reserved.
+//
+
+import XCTest
+@testable import DestroTheWorld
+
+class DestroTheWorldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
